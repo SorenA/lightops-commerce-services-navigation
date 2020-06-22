53ee9cf7709e216eb321826e70b1adaa37e5e10f
SorenA <s.dev@lysdal.org>
2020-06-22 22:58:18 +0200

    Add dependency injection component.

---
+++ src/LightOps.Commerce.Services.Navigation/Configuration/INavigationServiceComponent.cs
@@ -0,0 +1,19 @@
+﻿using LightOps.Commerce.Services.Navigation.Api.QueryHandlers;
+using LightOps.Commerce.Services.Navigation.Api.Services;
+
+namespace LightOps.Commerce.Services.Navigation.Configuration
+{
+    public interface INavigationServiceComponent
+    {
+        #region Services
+        INavigationServiceComponent OverrideNavigationService<T>() where T : INavigationService;
+        #endregion Services
+
+        #region Query Handlers
+        INavigationServiceComponent OverrideFetchNavigationsByParentIdQueryHandler<T>() where T : IFetchNavigationsByParentIdQueryHandler;
+        INavigationServiceComponent OverrideFetchNavigationsByRootQueryHandler<T>() where T : IFetchNavigationsByRootQueryHandler;
+        INavigationServiceComponent OverrideFetchNavigationByHandleQueryHandler<T>() where T : IFetchNavigationByHandleQueryHandler;
+        INavigationServiceComponent OverrideFetchNavigationByIdQueryHandler<T>() where T : IFetchNavigationByIdQueryHandler;
+        #endregion Query Handlers
+    }
+}
---
+++ src/LightOps.Commerce.Services.Navigation/Configuration/NavigationServiceComponent.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using LightOps.Commerce.Services.Navigation.Api.Models;
+using LightOps.Commerce.Services.Navigation.Api.Queries;
+using LightOps.Commerce.Services.Navigation.Api.QueryHandlers;
+using LightOps.Commerce.Services.Navigation.Api.Services;
+using LightOps.Commerce.Services.Navigation.Domain.Services;
+using LightOps.CQRS.Api.Queries;
+using LightOps.DependencyInjection.Api.Configuration;
+using LightOps.DependencyInjection.Domain.Configuration;
+
+namespace LightOps.Commerce.Services.Navigation.Configuration
+{
+    public class NavigationServiceComponent : IDependencyInjectionComponent, INavigationServiceComponent
+    {
+        public string Name => "lightops.commerce.services.navigation";
+
+        public IReadOnlyList<ServiceRegistration> GetServiceRegistrations()
+        {
+            return new List<ServiceRegistration>()
+                .Union(_services.Values)
+                .Union(_queryHandlers.Values)
+                .ToList();
+        }
+
+        #region Services
+        internal enum Services
+        {
+            NavigationService,
+        }
+
+        private readonly Dictionary<Services, ServiceRegistration> _services = new Dictionary<Services, ServiceRegistration>
+        {
+            [Services.NavigationService] = ServiceRegistration.Scoped<INavigationService, NavigationService>(),
+        };
+
+        public INavigationServiceComponent OverrideNavigationService<T>()
+            where T : INavigationService
+        {
+            _services[Services.NavigationService].ImplementationType = typeof(T);
+            return this;
+        }
+        #endregion Services
+
+        #region Query Handlers
+        internal enum QueryHandlers
+        {
+            FetchNavigationsByParentIdQueryHandler,
+            FetchNavigationsByRootQueryHandler,
+            FetchNavigationByHandleQueryHandler,
+            FetchNavigationByIdQueryHandler,
+        }
+
+        private readonly Dictionary<QueryHandlers, ServiceRegistration> _queryHandlers = new Dictionary<QueryHandlers, ServiceRegistration>
+        {
+            [QueryHandlers.FetchNavigationsByParentIdQueryHandler] = ServiceRegistration.Scoped<IQueryHandler<FetchNavigationsByParentIdQuery, IList<INavigation>>>(),
+            [QueryHandlers.FetchNavigationsByRootQueryHandler] = ServiceRegistration.Scoped<IQueryHandler<FetchNavigationsByRootQuery, IList<INavigation>>>(),
+            [QueryHandlers.FetchNavigationByHandleQueryHandler] = ServiceRegistration.Scoped<IQueryHandler<FetchNavigationByHandleQuery, INavigation>>(),
+            [QueryHandlers.FetchNavigationByIdQueryHandler] = ServiceRegistration.Scoped<IQueryHandler<FetchNavigationByIdQuery, INavigation>>(),
+        };
+
+        public INavigationServiceComponent OverrideFetchNavigationsByParentIdQueryHandler<T>() where T : IFetchNavigationsByParentIdQueryHandler
+        {
+            _queryHandlers[QueryHandlers.FetchNavigationsByParentIdQueryHandler].ImplementationType = typeof(T);
+            return this;
+        }
+
+        public INavigationServiceComponent OverrideFetchNavigationsByRootQueryHandler<T>() where T : IFetchNavigationsByRootQueryHandler
+        {
+            _queryHandlers[QueryHandlers.FetchNavigationsByRootQueryHandler].ImplementationType = typeof(T);
+            return this;
+        }
+
+        public INavigationServiceComponent OverrideFetchNavigationByHandleQueryHandler<T>() where T : IFetchNavigationByHandleQueryHandler
+        {
+            _queryHandlers[QueryHandlers.FetchNavigationByHandleQueryHandler].ImplementationType = typeof(T);
+            return this;
+        }
+
+        public INavigationServiceComponent OverrideFetchNavigationByIdQueryHandler<T>() where T : IFetchNavigationByIdQueryHandler
+        {
+            _queryHandlers[QueryHandlers.FetchNavigationByIdQueryHandler].ImplementationType = typeof(T);
+            return this;
+        }
+        #endregion Query Handlers
+    }
+}
